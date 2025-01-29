d40828210982d8d461da06b36b7cd44ca88709a5
MarlusFreitas <marlus_fox@hotmail.com>
2025-01-28 21:25:31 -0300

    Implementação do departamento e vendedor

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,13 +2,21 @@
 package application;
 
 import db.DB;
+import entities.Department;
+import entities.Seller;
 import java.sql.Connection;
+import java.time.LocalDate;
 
 public class Program {
 
     public static void main(String[] args) {
         
         Connection conn = DB.getConnection();
+        
+        Department dep = new Department(345, "marlus");
+        System.out.println(dep);
+        Seller sel = new Seller(123, "marlussssss", "marlus@gmail.com", LocalDate.now());
+        System.out.println(sel);
         DB.closeConnection();
     }
     
---
+++ src/entities/Seller.java
@@ -0,0 +1,100 @@
+package entities;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Seller implements Serializable{
+    
+    private Integer id;
+    private String name;
+    private String email;
+    private LocalDate birthDate;
+
+    public Seller(Integer id, String name, String email, LocalDate birthDate) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + Objects.hashCode(this.id);
+        hash = 79 * hash + Objects.hashCode(this.name);
+        hash = 79 * hash + Objects.hashCode(this.email);
+        hash = 79 * hash + Objects.hashCode(this.birthDate);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Seller other = (Seller) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.email, other.email)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return Objects.equals(this.birthDate, other.birthDate);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Seller{");
+        sb.append("id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append(", email=").append(email);
+        sb.append(", birthDate=").append(birthDate);
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    
+}
